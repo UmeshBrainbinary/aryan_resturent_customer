ddf312fca2790d9c2df7151bb323045406e41a17
UmeshBrainbinary <138089391+UmeshBrainbinary@users.noreply.github.com>
2024-03-20 10:25:51 +0530

    commit_code

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 final box = GetStorage();
 dynamic langValue = const Locale('en', null);
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
